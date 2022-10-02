feb3ea2d047461a1d5c9462f16e1b7e18a7ad9f2
Andrey Kislitsyn <kanvaweb@gmail.com>
2022-10-02 20:19:55 +0700

    Случайные числа

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿// Это моя вторая программа C#
-Console.Write("Введите Ваше имя: ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+// Console.Write("numberA: ");
+// int numberA = Console.ReadLine();
+// Console.Write("numberB: ");
+// int numberB = Console.Read();
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.Write("Сумма равна = ");
+Console.Write(result);
