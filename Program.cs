9ad22f9810bb568f255fd4159d85675ddadf9261
Andrey Kislitsyn <kanvaweb@gmail.com>
2022-10-02 21:05:40 +0700

    Ветвление

--- Program.cs
+++ Program.cs
@@ -3,10 +3,23 @@
 // int numberA = Console.ReadLine();
 // Console.Write("numberB: ");
 // int numberB = Console.Read();
-int numberA = new Random().Next(1, 10);
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.Write("Сумма равна = ");
-Console.Write(result);
+// int numberA = new Random().Next(1, 10);
+// Console.WriteLine(numberA);
+// int numberB = new Random().Next(1, 10);
+// Console.WriteLine(numberB);
+// int result = numberA + numberB;
+// Console.Write("Сумма равна = ");
+// Console.Write(result);
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
