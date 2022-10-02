98941bb6ec6fd2689de008940e7fe761570b3031
Andrey Kislitsyn <kanvaweb@gmail.com>
2022-10-02 21:32:20 +0700

    Треугольник

--- Program.cs
+++ Program.cs
@@ -24,19 +24,58 @@
 //     Console.Write(username);
 // }
 
-int a = 1;
-int b = 2;
-int c = 6;
-int d = 8;
-int e = 4;
+// int a = 1;
+// int b = 2;
+// int c = 6;
+// int d = 8;
+// int e = 4;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// if (d > max) max = d;
+// if (e > max) max = e;
+// Console.Write("max = ");
+// Console.WriteLine(max);
 
-int max = a;
-if (a > max) max = a;
-if (b > max) max = b;
-if (c > max) max = c;
-if (d > max) max = d;
-if (e > max) max = e;
+Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
 
-Console.Write("max = ");
+int xa = 1, ya = 1,
+    xb = 1, yb = 30,
+    xc = 60, yc = 30;
 
-Console.WriteLine(max);
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+int count = 0;
+while(count < 10000)
+{
+    int what = new Random().Next(0, 3);
+
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
